f2eefa6bac71f41646025fd10a02803972344066
Dionis Markov <ddmarrrkov@outlook.com>
2022-10-14 20:36:22 +0200

    Update Tile.jsx

--- components/Tile.jsx
+++ components/Tile.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Tile = ({ href }) => {
+const Tile = ({ href, logo }) => {
 	return (
 		<Link href={href}>
 			<a className='tile' target='_blank' rel='noopener noreferrer'>
+				<Image src={logo} alt='' />
 			</a>
 		</Link>
 	);
