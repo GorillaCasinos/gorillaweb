f3581153076f158c5ed68702489c8f8c7e4f8943
Dionis Markov <ddmarrrkov@outlook.com>
2022-10-21 16:56:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import { Blur, Navbar, Seo } from '../components';
+import {
+	Eclipses,
+	FAQ,
+	Footer,
+	Hero,
+	HowItWorks,
+	Qualities,
+	Roadmap,
+	Token,
+} from '../containers';
+
+export default function Home() {
+	return (
+		<div className='layout-container'>
+			<Seo />
+			<Eclipses />
+			<Navbar />
+			<Blur>
+				<Hero />
+				<Qualities />
+				<Token />
+				<Roadmap />
+				<HowItWorks />
+				<FAQ />
+			</Blur>
+			<Footer />
+		</div>
+	);
+}
