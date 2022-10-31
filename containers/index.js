735bdeb40e1dd244f38b5c62634cbb5e5aff1d6a
Dionis Markov <ddmarrrkov@outlook.com>
2022-11-01 00:04:29 +0100

    Add files via upload

--- containers/index.js
+++ containers/index.js
@@ -1,9 +1,18 @@
-
-export { default as Eclipses } from './Eclipses';
-export { default as FAQ } from './FAQ';
-export { default as Footer } from './Footer';
-export { default as Hero } from './Hero';
-export { default as HowItWorks } from './Fundamentals';
-export { default as Qualities } from './CoreStrength';
-export { default as Roadmap } from './Roadmap';
-export { default as Token } from './Token';
+export { default as Eclipses }
+from './Eclipses';
+export { default as FAQ }
+from './FAQ';
+export { default as Footer }
+from './Footer';
+export { default as Hero }
+from './Hero';
+export { default as HowItWorks }
+from './Fundamentals';
+export { default as Qualities }
+from './CoreStrength';
+export { default as Roadmap }
+from './Roadmap';
+export { default as Token }
+from './Token';
+export { default as Team }
+from './Team'
